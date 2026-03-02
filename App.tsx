94346cf54d717a6cf751c4bef18175758ed6136c
yash <yashrajsinhjadeja2253@gmail.com>
2026-03-02 11:19:07 +0530

    first 2 views done

--- App.tsx
+++ App.tsx
@@ -12,7 +12,25 @@ function App() {
   return (
     <SafeAreaProvider style={styles.primaryView}>
       <SafeAreaView>
-        <View style={styles.subView}></View>
+        {/* ==== First View ==== */}
+        <View style={styles.firstView}>
+          <View>
+            <View style={styles.redBox}></View>
+          </View>
+          <View>
+            <View style={styles.teal}></View>
+          </View>
+        </View>
+        {/* ==== Second View ==== */}
+        <View style={styles.firstView}>
+          <View>
+            <View style={styles.yellowBox}></View>
+          </View>
+          <View>
+            <View style={styles.pinkBox}></View>
+          </View>
+        </View>
+        {/* ==== Third View ==== */}
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -21,20 +39,41 @@ function App() {
 const styles = StyleSheet.create({
   //MainView
   primaryView: {
-    backgroundColor: '#6367FF',
-    flex: 1,
-    padding: 2,
-  },
-  //secondView
-  subView: {
     backgroundColor: '#FFFFFF',
     flex: 1,
+    padding: 10,
   },
+  //First View
+  firstView: {
+    margin: 10,
+    flexDirection: 'row',
+    gap: 15,
+    alignSelf: 'flex-end',
+  },
+
   //Red Box
   redBox: {
     backgroundColor: '#FF0000',
-    width: 50,
-    height: 50,
+    width: 100,
+    height: 100,
+  },
+  //Teal Box first
+  teal: {
+    backgroundColor: '#9CD5FF',
+    width: 250,
+    height: 100,
+  },
+  //pink Box
+  pinkBox: {
+    backgroundColor: '#FFA6A6',
+    width: 100,
+    height: 100,
+  },
+  //yello box
+  yellowBox: {
+    backgroundColor: '#FFD150',
+    width: 250,
+    height: 100,
   },
 });
 
